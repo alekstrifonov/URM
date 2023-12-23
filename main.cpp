8657a1edd9ff67c6d9114a0c1c54175195b394bd
Alex Trifonov <alekstrifonov64@gmail.com>
2023-12-23 10:30:48 +0200

    added 1 test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+
 #include <iostream>
+#include "doctest.h"
+
+TEST_CASE("Test Doctest")
+{
+    CHECK(1 == 1);
+}
 
 int main()
 {
-    std::cout << "Repository is set up";
+    doctest::Context().run();
 }
