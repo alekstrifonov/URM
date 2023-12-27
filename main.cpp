59b25f7fe031a9348997014d8b705e84be750eb6
Alex Trifonov <alekstrifonov64@gmail.com>
2023-12-27 07:22:21 +0200

    Added Sparse Array tests

--- main.cpp
+++ main.cpp
@@ -2,10 +2,56 @@
 
 #include <iostream>
 #include "doctest.h"
+#include "SparseArray.hpp"
 
-TEST_CASE("Test Doctest")
+TEST_CASE("TEST SPARSE ARRAY BASIC FUNCTIONS")
 {
-    CHECK(1 == 1);
+    SparseArray a;
+
+    std::vector<unsigned int> b = {1, 0, 0, 5, 0, 4, 3};
+
+    a.add(b);
+    std::size_t j = 0;
+
+    for (std::size_t i = 0; i < b.size(); ++i)
+    {
+        if (b[i] != 0)
+        {
+            CHECK(b[i] == a.getValues()[j]);
+            CHECK(i == a.getIndexes()[j]);
+            ++j;
+        }
+    }
+}
+
+TEST_CASE("TEST URM INSTRUCTIONS")
+{
+    SparseArray a;
+
+    std::vector<unsigned int> b = {1,2,3};
+
+    a.INC(0);
+    a.INC(1);
+    a.INC(1);
+    a.INC(2);
+    a.INC(2);
+    a.INC(2);
+
+    CHECK(a.size() ==  b.size());
+
+    for (std::size_t i = 0; i < a.size(); i++)
+    {
+        CHECK(a.getValues()[i] == b[i]);
+        CHECK(a.getIndexes()[i] == i);
+    }
+    
+    a.ZERO(0);
+    CHECK(a.size() != b.size());
+
+    a.ZERO(1);
+    a.ZERO(2);
+
+    CHECK(a.size() == 0);
 }
 
 int main()
