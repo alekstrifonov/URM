96a8ccca010f78206eba40715dbbc15b01e78100
Alex Trifonov <alekstrifonov64@gmail.com>
2023-12-28 10:16:28 +0200

    Added tests for basic instruction tokenization

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include "doctest.h"
 #include "SparseArray.hpp"
+#include "tokenizer.hpp"
 
 void compare(const SparseArray &a, const std::vector<unsigned int> &b)
 {
@@ -43,7 +44,7 @@ TEST_CASE("TEST ZERO AND INC")
 
     CHECK(a.size() == b.size());
 
-    compare(a,b);
+    compare(a, b);
 
     a.ZERO(0);
     CHECK(a.size() != b.size());
@@ -78,6 +79,43 @@ TEST_CASE("TEST MOVE")
     std::vector<unsigned int> c = {2, 2, 1};
 
     compare(a, c);
+}
+
+TEST_CASE("Test Basic Operators Tokenization")
+{
+    std::istringstream is("ZERO 1\n INC 2\nMOVE 1 2\nJUMP 3\nJUMP 1 3 8\n");
+    Tokenizer tokenizer(is);
+
+    Tokenizer::Token token;
+
+        is >> token;
+
+        CHECK(token.type == Tokenizer::ZERO);
+        CHECK(token.value.size() == 1);
+        CHECK(token.value[0] == 1);
+
+        is >> token;
+        CHECK(token.type == Tokenizer::INC);
+        CHECK(token.value.size() == 1);
+        CHECK(token.value[0] == 2);
+
+        is >> token;
+        CHECK(token.type == Tokenizer::MOVE);
+        CHECK(token.value.size() == 2);
+        CHECK(token.value[0] == 1);
+        CHECK(token.value[1] == 2);
+
+        is >> token;
+        CHECK(token.type == Tokenizer::JUMP);
+        CHECK(token.value.size() == 1);
+        CHECK(token.value[0] == 3);
+
+        is >> token;
+        CHECK(token.type == Tokenizer::IF_JUMP);
+        CHECK(token.value.size() == 3);
+        CHECK(token.value[0] == 1);
+        CHECK(token.value[1] == 3);
+        CHECK(token.value[2] == 8);
 }
 
 int main()
