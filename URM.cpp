efb29a7825f8c2fa98dcbb1c420336d869f94b18
Alex Trifonov <alekstrifonov64@gmail.com>
2023-12-29 07:32:10 +0200

    added the rest of the cases

--- URM.cpp
+++ URM.cpp
@@ -11,7 +11,6 @@ URM::URM(const Tokenizer::Token &t)
 
 void URM::loadFromFile(std::ifstream &is)
 {
-
 }
 
 void URM::dialogue()
@@ -24,13 +23,63 @@ void URM::print() const
 
 void URM::evaluate()
 {
-    if (currentInstruction.type == Tokenizer::ZERO)
+    switch (currentInstruction.type)
     {
+    case Tokenizer::ZERO:
         memory.ZERO(currentInstruction.value[0]);
-    } else if (currentInstruction.type == Tokenizer::INC)
-    {
+        break;
+
+    case Tokenizer::INC:
         memory.INC(currentInstruction.value[0]);
+        break;
+
+    case Tokenizer::MOVE:
+        memory.MOVE(currentInstruction.value[0], currentInstruction.value[1]);
+        break;
+
+    case Tokenizer::JUMP:
+        /* code */
+        break;
+
+    case Tokenizer::IF_JUMP:
+        /* code */
+        break;
+    case Tokenizer::RANGE_ZERO:
+        /* code */
+        break;
+
+    case Tokenizer::SET:
+        /* code */
+        break;
+    case Tokenizer::COPY:
+        /* code */
+        break;
+
+    case Tokenizer::MEM:
+        /* code */
+        break;
+    case Tokenizer::LOAD:
+        /* code */
+        break;
+
+    case Tokenizer::RUN:
+        /* code */
+        break;
+    case Tokenizer::ADD:
+        /* code */
+        break;
+
+    case Tokenizer::QUOTE:
+        /* code */
+        break;
+    case Tokenizer::CODE:
+        /* code */
+        break;
+    case Tokenizer::COMMENT:
+        /* code */
+        break;
+
+    default:
+        break;
     }
-    
-    
 }
